a067ad67af3c7795ce184011f7791e0b1f9f0626
Quynhnga <quynhnga@ubuntu.ubuntu-domain>
2013-03-31 09:41:07 +0700

    init empty project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gardian::Application.config.secret_token = '60548ba8ea2a1089290e90becdcfd2c21f188bc32d438affb69b85fa92ac479f49f5ed4bd397536f6e23ae48be690fe7b687d4fc7e859b11eb6c5f4d5a338747'
